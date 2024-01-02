9fb5d673584e0af5a15588c77117211e36de21ce
luozhiping <luozhiping.xidian@gmail.com>
2024-01-02 21:56:32 +0800

    push

--- data_prepare_win.py
+++ data_prepare_win.py
@@ -8,7 +8,7 @@ all_file = []
 
 step = 1
 
-for root, dirs, files in os.walk('.\data\\11_29', topdown=False):
+for root, dirs, files in os.walk('.\collect\\output\\12_25', topdown=False):
     for name in files:
         if not name.endswith('.pkl'):
             continue
@@ -17,26 +17,33 @@ for root, dirs, files in os.walk('.\data\\11_29', topdown=False):
 print(all_file)
 
 epsoides = []
-max_length = 60
+max_length = 32
 all_show = []
 min_p = np.array([-30, 0, 0, -30, 20, 0])
 max_p = np.array([30, 100, 100, 10, 100, 1])
+min_p = 900
+max_p = 1350
 for file in all_file:
     datas = pickle.load(open(file, 'rb'))
     print(len(datas))
-    epsoide = {'name': file.split("\\")[-1].split(".")[0],
+    epsoide = {'name': file.split("/")[-1].split(".")[0],
                'angle_offset': [],
                'angle_pos': [],
                'gripper': [],
+               'angle': [],
+               'radians': [],
+               'gripper_pos': [],
+               'radians_pos': [],
                'image': {
-                   'wrist':[],
+                   'wrist': [],
                    'front': [],
                    'right': [],
                    'top': []
                }}
     if (len(datas)) < max_length:
         continue
-    for i in range(max_length):
+    for j in range(max_length):
+        i = j + 1
         data = datas[i]
         if i < len(datas) - step:
             next_data = datas[i + step]
@@ -48,12 +55,17 @@ for file in all_file:
         gripper = data['gripper_status']
         # print(angle_offset, gripper, data['gripper_pos'])
         epsoide['angle_offset'].append(angle_offset)
+        epsoide['angle'].append(np.array(next_robot_status['jointAngle']))
+        epsoide['radians'].append(np.radians(next_robot_status['jointAngle']))
+        epsoide['radians_pos'].append(np.radians(robot_status['jointAngle']))
+
         epsoide['gripper'].append([gripper])
-
-        jointAngle = (np.clip(np.array(robot_status['jointAngle']), min_p, max_p) - min_p) / (max_p - min_p)
-        # all_show.append(jointAngle)
-
-        epsoide['angle_pos'].append(jointAngle)
+        epsoide['gripper_pos'].append([(int(next_data['gripper_pos']) - min_p) / (max_p - min_p)])
+        # print(np.radians(next_robot_status['jointAngle']), np.radians(robot_status['jointAngle']))
+        all_show.append(np.radians(next_robot_status['jointAngle']))
+        # jointAngle = (np.clip(np.array(robot_status['jointAngle']), min_p, max_p) - min_p) / (max_p - min_p)
+        #
+        # epsoide['angle_pos'].append(jointAngle)
         filename = file.split("\\")[-1].split(".")[0]
         epsoide['image']['wrist'].append('./image/%s_wrist_%s.jpg' % (filename, i))
         epsoide['image']['front'].append('./image/%s_front_%s.jpg' % (filename, i))
@@ -72,12 +84,12 @@ for file in all_file:
     epsoides.append(epsoide)
     # break
 
-# print(np.array(all_show).shape)
-# plt.figure(1)
-# plt.hist(np.array(all_show)[:, 2], bins=256, facecolor="blue", edgecolor="black", alpha=0.7)
-# plt.xlabel("x")
-# plt.ylabel("dis")
-# plt.show()
+print(np.array(all_show)[:, 1].shape)
+plt.figure(1)
+plt.hist(np.array(all_show)[:, 1], bins=256, facecolor="blue", edgecolor="black", alpha=0.7)
+plt.xlabel("x")
+plt.ylabel("dis")
+plt.show()
 # -30, 30
 # 0, 100
 # 0, 100
--- utils.py
+++ utils.py
@@ -10,176 +10,13 @@ import random
 import glob
 import pickle
 from PIL import Image
-ACTION_ANGLE = 'angle'
-ACTION_GRIPPER  ='gripper_pos'
+ACTION_ANGLE = 'angle_offset'
+ACTION_GRIPPER  ='gripper'
 ACTION_ANGLE_POS = 'radians_pos'
 
-class EpisodicDatasetRobopen2(torch.utils.data.Dataset):
-    def __init__(self, episode_ids, dataset_dir, norm_stats,num_episodes):
-        super(EpisodicDatasetRobopen).__init__()
-        self.episode_ids = episode_ids
-        self.dataset_dir = dataset_dir
-        self.norm_stats = norm_stats
-        self.num_episodes = num_episodes
-        self.is_sim = None
-        self.h5s = []
-        self.trials = []
-        self.task_emb_per_trial = []
-        self.verbose = True
-        self.h5s = {}
-        lens = []
-        
-        files = list()
-        randfiles = list()
-        n = 100
-        for i in glob.glob("{}/".format(dataset_dir)):
-            print(i)
-            randfiles.append(sorted(glob.glob(os.path.join(i, "*.h5"))))
-
-        for i in randfiles:
-            l = len(i)
-            num = int(n/250 * l)
-            num = 1
-            for file in range(num):
-                files.append(i[file])
-
-        files = sorted(files)
-
-        # files = sorted(glob.glob(os.path.join(dataset_dir + "*/*/", '*.h5')))
-        for filename in files:
-            #for 20 tasks hardcoded, modify as needed
-            if 'open_drawer' in filename:
-                task_emb = TEXT_EMBEDDINGS[0]
-            elif 'close_drawer' in filename:
-                task_emb = TEXT_EMBEDDINGS[1]
-            elif 'pick_butter' in filename:
-                task_emb = TEXT_EMBEDDINGS[2]            
-            elif 'place_butter' in filename:
-                task_emb = TEXT_EMBEDDINGS[3]
-            elif 'pick_toast' in filename:
-                task_emb = TEXT_EMBEDDINGS[4]
-            elif 'place_toast' in filename:
-                task_emb = TEXT_EMBEDDINGS[5] 
-            elif 'cap_lid' in filename:
-                task_emb = TEXT_EMBEDDINGS[6] 
-            elif 'pick_lid' in filename:
-                task_emb = TEXT_EMBEDDINGS[7]
-            elif 'pick_tea' in filename:
-                task_emb = TEXT_EMBEDDINGS[8]
-            elif 'place_lid' in filename:
-                task_emb = TEXT_EMBEDDINGS[9] 
-            elif 'place_tea' in filename:
-                task_emb = TEXT_EMBEDDINGS[10]
-            elif 'uncap_lid' in filename:
-                task_emb = TEXT_EMBEDDINGS[11]
-            elif 'close_oven' in filename:
-                task_emb = TEXT_EMBEDDINGS[12]
-            elif 'open_oven' in filename:
-                task_emb = TEXT_EMBEDDINGS[13]
-            elif 'place_bowl' in filename:
-                task_emb = TEXT_EMBEDDINGS[14]
-            elif 'slide_out' in filename:
-                task_emb = TEXT_EMBEDDINGS[15]
-            elif "cap_mug" in filename:
-                task_emb = TEXT_EMBEDDINGS[16]
-            elif "pick_mug" in filename:
-                task_emb = TEXT_EMBEDDINGS[17]
-            elif "pick_towel" in filename:
-                task_emb = TEXT_EMBEDDINGS[18]
-            elif "wipe_towel" in filename:
-                task_emb = TEXT_EMBEDDINGS[19]
-            elif "pick_cup" in filename:
-                task_emb = TEXT_EMBEDDINGS[20]
-            elif "place_cup" in filename:
-                task_emb = TEXT_EMBEDDINGS[21]
-            else:
-                task_emb = TEXT_EMBEDDINGS[0]
-                'SINGLE TASK embedding wont be used'
-
-            h5 = h5py.File(filename, 'r')
-            for key, trial in h5.items():
-                if(trial['data']['time'].shape[0] != 42):
-                    continue
-                # Open the trial and extract metadata
-                lens.append(trial['data']['ctrl_arm'].shape[0])
-                # Bookkeeping for all the trials
-                self.trials.append(trial)
-                self.task_emb_per_trial.append(task_emb)
-
-        self.trial_lengths = np.cumsum(lens)
-        self.max_idx = self.trial_lengths[-1]
-        print("TOTAL TRIALS",len(self.trials))
-        self.trials = self.trials[:num_episodes]
-
-        # assert self.num_episodes == len(self.trials) ## sanity check that all files are loaded, remove if needed
-
-        print('TOTAL TRIALS = num_episodes = ',len(self.trials))
-        self.__getitem__(0)
-
-
-    def __len__(self):
-        return len(self.episode_ids)
-
-    def __getitem__(self, idx):
-        sample_full_episode = False # hardcode
-        trial_idx = self.episode_ids[idx]
-        trial = self.trials[trial_idx]
-        task_emb = self.task_emb_per_trial[trial_idx]
-        camera_names = CAMERA_NAMES
-
-        action = np.concatenate([trial['data']['ctrl_arm'], trial['data']['ctrl_ee']], axis=1).astype(np.float32)
-        original_action_shape = action.shape
-        cutoff = 2 #10#5 
-        episode_len = original_action_shape[0] -cutoff ## cutoff last few
-
-        if sample_full_episode:
-            start_ts = 0
-        else:
-            start_ts = np.random.choice(episode_len)
-        # get observation at start_ts only
-        qpos = trial['data']['qp_arm'][start_ts].astype(np.float32)
-        qvel = trial['data']['qv_arm'][start_ts].astype(np.float32)
-
-        image_dict = dict()
-        for cam_name in camera_names:
-            image_dict[cam_name] = trial['data'][f'{cam_name}'][start_ts]
-        # get all actions after and including start_ts
-        action = np.concatenate([trial['data']['ctrl_arm'], trial['data']['ctrl_ee']], axis=1)[max(0, start_ts - 1):].astype(np.float32) # hack, to make timesteps more aligned
-        action_len = episode_len - max(0, start_ts - 1) # hack, to make timesteps more aligned 
-
-        padded_action = np.zeros(original_action_shape, dtype=np.float32)
-        padded_action[:action_len] = action[:-cutoff]
-        is_pad = np.zeros(episode_len)
-        is_pad[action_len:] = 1
-
-        # new axis for different cameras
-        all_cam_images = []
-        for cam_name in camera_names:
-            all_cam_images.append(image_dict[cam_name])
-        all_cam_images = np.stack(all_cam_images, axis=0)
-
-        # construct observations
-        image_data = torch.from_numpy(all_cam_images)
-        qpos_data = torch.from_numpy(qpos).float()
-        action_data = torch.from_numpy(padded_action).float()
-        is_pad = torch.from_numpy(is_pad).bool()
-
-        # channel last
-        image_data = torch.einsum('k h w c -> k c h w', image_data)
-
-        # normalize image and change dtype to float
-        image_data = image_data / 255.0
-        action_data = (action_data - self.norm_stats["action_mean"]) / self.norm_stats["action_std"]
-        qpos_data = (qpos_data - self.norm_stats["qpos_mean"]) / self.norm_stats["qpos_std"]
-
-        task_emb = torch.from_numpy(np.asarray(task_emb)).float()
-
-        return image_data, qpos_data, action_data, is_pad, task_emb
-
-
-class EpisodicDatasetRobopen(torch.utils.data.Dataset):
+class EpisodicDatasetRobopenTrain(torch.utils.data.Dataset):
     def __init__(self, episode_ids, dataset_dir, norm_stats, num_episodes, sample = False):
-        super(EpisodicDatasetRobopen).__init__()
+        super(EpisodicDatasetRobopenTrain).__init__()
         self.episode_ids = episode_ids
         self.dataset_dir = dataset_dir
         self.norm_stats = norm_stats
@@ -371,9 +208,9 @@ class EpisodicDatasetRobopenVal(torch.utils.data.Dataset):
 
         return image_data, qpos_data2, action_data2, is_pad, task_emb
 
-class EpisodicDatasetRobopen3(torch.utils.data.Dataset):
+class EpisodicDatasetRobopenTest(torch.utils.data.Dataset):
     def __init__(self, episode_ids, dataset_dir, norm_stats, num_episodes, sample = False):
-        super(EpisodicDatasetRobopen3).__init__()
+        super(EpisodicDatasetRobopenTest).__init__()
         self.episode_ids = episode_ids
         self.dataset_dir = dataset_dir
         self.norm_stats = norm_stats
@@ -539,7 +376,7 @@ def load_data(dataset_dir, num_episodes, batch_size_train, batch_size_val, sampl
     # obtain normalization stats for qpos and action
     norm_stats = get_norm_stats_robopen(dataset_dir, num_episodes)
     # construct dataset and dataloader
-    train_dataset = EpisodicDatasetRobopen(train_indices, dataset_dir, norm_stats,num_episodes, sample)
+    train_dataset = EpisodicDatasetRobopenTrain(train_indices, dataset_dir, norm_stats, num_episodes, sample)
     val_dataset = EpisodicDatasetRobopenVal(val_indices, dataset_dir, norm_stats,num_episodes, sample)
 
     train_dataloader = DataLoader(train_dataset, batch_size=batch_size_train, shuffle=True, pin_memory=True)
@@ -556,7 +393,7 @@ def load_test_data(dataset_dir, num_episodes, batch_size_train, batch_size_val, 
     # obtain normalization stats for qpos and action
     # norm_stats = get_norm_stats_robopen(dataset_dir, num_episodes)
     # construct dataset and dataloader
-    train_dataset = EpisodicDatasetRobopen3(train_indices, dataset_dir, norm_stats,num_episodes, sample)
+    train_dataset = EpisodicDatasetRobopenTest(train_indices, dataset_dir, norm_stats, num_episodes, sample)
     # val_dataset = EpisodicDatasetRobopen3(val_indices, dataset_dir, norm_stats,num_episodes, sample)
 
     train_dataloader = DataLoader(train_dataset, batch_size=batch_size_train, shuffle=True, pin_memory=True)
